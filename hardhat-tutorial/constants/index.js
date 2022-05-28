c4c10c641c6ba9930ccc18f17f9db6225e5b909d
darshan_gondaliya <darshan.gondaliya@evoortsolutions.com>
2022-05-28 19:25:58 +0530

    hardhat commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x26ab8dD91921ef99816297Be7CF9c66B2B358F6e";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
